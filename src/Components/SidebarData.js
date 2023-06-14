e2e9c5fb85d0f139f18c03d94634072dd59aedec
radeon2525 <e1070572@u.nus.edu>
2023-06-14 19:01:36 +0530

    Sidebar

---
+++ src/Components/SidebarData.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export const SidebarData=[
+    {
+        title: "Item 1",
+        link: "/item1"
+    },
+    {
+        title: "Item 2",
+        link: "/item2"
+    },
+    {
+        title: "Item 3",
+        link: "/item3"
+    },
+    {
+        title: "Item 4",
+        link: "item4"
+    },
+    {
+        title: "Item 5",
+        link: "item5"
+    },
+    {
+        title: "Item 6",
+        link: "item6"
+    },
+    {
+        title: "Item 7",
+        link: "item7"
+    },
+    {
+        title: "Item 8",
+        link: "item8"
+    }
+]
